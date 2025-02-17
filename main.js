99a9ccb6b2eacfcac51bbdb73e2b3b15304af997
Tanecodes <tanecodes@gmail.com>
2025-02-17 13:10:12 +1000

    implemented some js to take user input and use the add button to display text

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const addButton = document.getElementById("addbutton");
 const userInput = document.getElementById("userinput");
+const todoResults = document.getElementById("todoresults");
 
 addButton.addEventListener("click",function(){
-  
+  let getInput = userInput.value.trim();
+  if(getInput){
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("todoresults");
+    newDiv.textContent = getInput;
+    todoResults.appendChild(newDiv);
+    userInput.value = "";
+  }
 });
