1c670d3016b246e1ccfd67c2fa698d6843fd528e
Tanecodes <tanecodes@gmail.com>
2025-02-18 20:42:07 +1000

    added complete button for bubbles item and extracted callback function to a normal function, implemented the enter key press for alternative to the add button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const addButton = document.getElementById("addbutton");
 const userInput = document.getElementById("userinput");
 const todoResults = document.getElementById("todoresults");
 
-addButton.addEventListener("click",function(){
+function bubble(){
 
   const getInput = userInput.value.trim();
 
@@ -12,8 +12,24 @@ addButton.addEventListener("click",function(){
   item.classList.add("todoresults");
   item.textContent = getInput;
 
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("completebtn");
+  completeBtn.textContent = "complete"
+
+  completeBtn.addEventListener("click",function(){
+    item.remove();
+  });
+
   todoResults.appendChild(item);
+  item.appendChild(completeBtn);
   
   userInput.value = "";
-  
+   
+};
+
+addButton.addEventListener("click",bubble);
+userInput.addEventListener("keydown",function(enter){
+  if(enter.key === "Enter"){
+    bubble();
+  }
 });
