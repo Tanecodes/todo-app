d620a0119cb32f57567c1a1df4fc464de0a49349
Tanecodes <tanecodes@gmail.com>
2025-02-19 23:22:15 +1000

    implemented local storage woohoo took me a while still have no idea how it works though but i got it done

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
+// constant variables.
 const addButton = document.getElementById("addbutton");
 const userInput = document.getElementById("userinput");
 const todoResults = document.getElementById("todoresults");
+// saving bubble text to local storage
+function saveBubbles() {
+  let storageArray = [];
+  document.querySelectorAll(".todoresults").forEach( value => {
+    let bubbleText = value.textContent.replace("complete", "").trim();
+    storageArray.push(bubbleText);
+  });
+  localStorage.setItem("todoItems",JSON.stringify(storageArray));
+};
+// loading bubble text from local storage.
+function loadBubbles() {
+  let savedBubbles = JSON.parse(localStorage.getItem("todoItems")) || [];
+  if (savedBubbles && Array.isArray(savedBubbles)){
+    savedBubbles.forEach(bubbleText => {
+      bubble(bubbleText);
+    });
+  }
+};
 
-function bubble(){
-
-  const getInput = userInput.value.trim();
-
-  if(!getInput) return;
+// function to create the todo bubbles.
+function bubble(bubbleText) {
+  let getInput = bubbleText || userInput.value.trim();
+  if(getInput === "") return;
 
   const item = document.createElement("div");
   item.classList.add("todoresults");
-  item.textContent = getInput;
+  
+  const bubbleTextElement = document.createElement("span");
+  bubbleTextElement.textContent = getInput;
 
   const completeBtn = document.createElement("button");
   completeBtn.classList.add("completebtn");
@@ -18,18 +38,30 @@ function bubble(){
 
   completeBtn.addEventListener("click",function(){
     item.remove();
+    saveBubbles();
   });
 
-  todoResults.appendChild(item);
+  item.appendChild(bubbleTextElement);
   item.appendChild(completeBtn);
-  
+  todoResults.appendChild(item);  
+
+  saveBubbles();
+
   userInput.value = "";
    
 };
 
-addButton.addEventListener("click",bubble);
+document.addEventListener("DOMContentLoaded", loadBubbles);
+
+// adds event listener to add button when clicked.
+addButton.addEventListener("click",function() {
+  bubble(userInput.value.trim());
+});
+
+// adds an event listener to the input bar so you can press the enter key
 userInput.addEventListener("keydown",function(enter){
   if(enter.key === "Enter"){
-    bubble();
+    bubble(userInput.value.trim());
   }
 });
+
